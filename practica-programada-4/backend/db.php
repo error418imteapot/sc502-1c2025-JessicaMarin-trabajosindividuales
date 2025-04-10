af33bed81aa6737d9e5d7e78bb13f28c4bec3117
Jessica Marín <114964794+error418imteapot@users.noreply.github.com>
2025-04-10 12:29:12 -0600

    pasar practica 4
    recordar subirlo aqui!!!!

---
+++ practica-programada-4/backend/db.php
@@ -0,0 +1,14 @@
+<?php
+$host = "localhost";
+$dbname = "todo_app";
+$user= "root";
+$password = "Jm12345";
+try{
+
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname",$user,$password);
+    $pdo -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    // echo "Conexion exitosa ". PHP_EOL;
+    
+}catch(PDOException $e){
+    die("Error de conexion: " . $e -> getMessage());
+}
